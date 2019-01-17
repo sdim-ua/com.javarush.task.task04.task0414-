c014883dee45e2b28b36e2060f397394bc2ecd68
sdim-ua <ingersol@yandex.ua>
2019-01-17 23:38:20 +0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,22 +4,24 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        
+
      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-     
+
    int numb = Integer.parseInt(reader.readLine());
+
+
+    if ( (numb  % 4 == 0 ) && (numb  % 100 != 0 || numb  % 400 == 0)){
+        int x = 366;
+         System.out.println("количество дней в году: " + x);
+     }
+    
+     else   {
+         int x = 365;
+        System.out.println("количество дней в году: " + x);
+   }  
    
-   if (numb / 4 == 0  ){
-       System.out.println("Високосный год");
-        if (numb % 100 == 0 & numb % 400 !=0){
-       System.out.println("НЕ Високосный год");      
-        }
-    else {
-         System.out.println("НЕ Високосный год");      
-    }
-   }
-  
+
 }  
-   
+
 
 }
